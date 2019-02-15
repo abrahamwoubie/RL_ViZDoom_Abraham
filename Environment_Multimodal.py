8a7f0a7561367fae88a3034df7354d44dc0ac7be
Abraham Woubie <abraham.zewoudie@uef.fi>
2019-02-15 20:08:58 +0200

    Update

--- Environment_Multimodal.py
+++ Environment_Multimodal.py
@@ -7,13 +7,11 @@ from vizdoom import *
 from pydub import AudioSegment
 from pydub.playback import play
 from ExtractFeatures import Extract_Features
-from GlobalVariables import GlobalVariables
 
 import scipy
 target_position_x = 100
 target_position_y = 100
 
-parameter=GlobalVariables
 Extract=Extract_Features
 
 class Environment_Multimodal(object):
